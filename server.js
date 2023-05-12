870cb22f683f0cf5c91e6b91c8257d4e9ab21683
Marco Bravo <proyectomarcobravo@gmail.com>
2023-05-12 10:40:32 +0200

    standard routes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors({ origin: "https://fantasticfy-products.netlify.app/" }));
+app.use(cors({ origin: "https://fantasticfy-products.netlify.app" }));
 
 app.get("/products", (req, res) => {
   const options = {
